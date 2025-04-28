d1c961812a825b7f25fcd452768b917c2a851fab
Yargelysv <127507310+Yargelysv@users.noreply.github.com>
2025-04-28 17:10:19 -0400

    titulo pagina

--- app/views/Tecnico/consulta_rif/index.php
+++ app/views/Tecnico/consulta_rif/index.php
@@ -9,7 +9,7 @@ include 'backEnd.php';
         <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
         <link rel="icon" type="image/png" href="../assets/img/favicon.png">
         <title>
-            Argon Dashboard 3 by Creative Tim
+            Soporte Post Venta
         </title>
         <!--     Fonts and icons     -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
