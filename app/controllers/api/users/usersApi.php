4d505523d8102f232ee7eca9fcfd8c0170201e54
IntelisoftD <desarrollo@inteligensa.com>
2026-01-13 14:36:56 -0400

    Revert "1) Se agrego el cambio para que en el modulo de documentos la persona encargada del pago de anticipo tien que ser del area de tesoreria y el rol administrador, y para que salga los filtro de exoneracion debe ser del area de taller boleita y el rol administrador"
    This reverts commit 538b1a2707414ad42fbd62ca694ea1c120574f34.

--- app/controllers/api/users/usersApi.php
+++ app/controllers/api/users/usersApi.php
@@ -284,8 +284,6 @@ class users extends Controller {
     $_SESSION['id_rol']        = (int) $userData['codtipousuario'];
     $_SESSION['name_rol']     = $userData['name_rol'];
     $_SESSION['status']       = (int) $userData['status'];
-    $_SESSION['id_area']      = (int) $userData['id_area'];
-
     
     $session_lifetime = 3600; // Ejemplo: 1200seg = 20 minutos
                               //          1800seg = 30 minutos
